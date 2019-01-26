75ce908674594ed0a2b7a4875032d00cf2785492
Tomas Carnecky <tomc@caurea.org>
2019-01-26 14:58:55 +0100

    Add ChannelPoint to Channel

--- src/resolvers/Channel.js
+++ src/resolvers/Channel.js
@@ -9,6 +9,15 @@ exports.channelId = ({ id }) => {
   return id;
 };
 
+exports.channelPoint = async ({ id }, _args, context) => {
+  const c = await lookupChannel(context, id);
+  return {
+    id: `${c.transaction_id}:${c.transaction_vout}`,
+    transaction: { id: c.transaction_id, txid: c.transaction_id },
+    outputIndex: c.transaction_vout
+  };
+};
+
 exports.capacity = async ({ id }, _args, context) => {
   const c = await lookupChannel(context, id);
   return c.capacity;
